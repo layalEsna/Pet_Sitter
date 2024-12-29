26bdb4a82adeb515348259c0cc98cc90b54b0978
Amene <layla_1380@yahoo.com>
2024-12-29 12:10:47 -0600

    Create SignupForm function with form state keys to match backend variable names (user_name, password, confirm_password, pet_name, pet_type, zip_code)

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
+
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignupForm from "./SignupForm";
+
 
 function App() {
   return (
---
+++ client/src/SignupForm.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function SignupForm() {
+    
+    const [formData, setFormData] = useState({
+        user_name: '',
+        password: '',
+        confirm_password: '',
+        pet_name: '',
+        pet_type: '',
+        zip_code: ''
+
+    })
+}
