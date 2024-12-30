e2fe4d517e726adcf4a70eb8d285c438faf4d2c5
Amene <layla_1380@yahoo.com>
2024-12-29 20:59:56 -0600

    Complete SignupForm component with form fields, dropdown for pet type, and error handling

--- client/src/SignupForm.js
+++ client/src/SignupForm.js
@@ -32,16 +32,115 @@ function SignupForm() {
             zip_code: Yup.string()
                 .matches(/^\d{5}$/, 'Zip code must be a valid 5-digit number')
                 .required('Zip code is requied')
-             })
+        }),
+        onSubmit: (values) => {
+            console.log(`Form data: ${values}`)
+        }
     })
     
-    
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <label htmlFor='user_name'>Create username</label>
+                <input
+                    id='user_name'
+                    type='text'
+                    name='user_name'
+                    value={formik.values.user_name}
+                    onChange={formik.handleChange}
+                />
+                {formik.errors.user_name && formik.touched.user_name && (
+                    <div>{ formik.errors.user_name}</div>
+                )}
+            </div>
+            <div>
+                <label htmlFor='password'>Create password</label>
+                <input
+                    id='password'
+                    type='password'
+                    name='password'
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                />
+                <div>
+                    {formik.errors.password && formik.touched.password && (
+                        <div>{ formik.errors.password}</div>
+                    )}
+                </div>
+            </div>
+            <div>
+                <label htmlFor='confirm_password'>Confirm password</label>
+                <input
+                    id='confirm_password'
+                    type='password'
+                    name='confirm_password'
+                    value={formik.values.confirm_password}
+                    onChange={formik.handleChange}
+                />
+                <div>
+                    {formik.errors.confirm_password && formik.touched.confirm_password && (
+                        <div>{ formik.errors.confirm_password}</div>
+                    )}
+                </div>
+            </div>
+            <div>
+                <label htmlFor='pet_name'>Pet name</label>
+                <input
+                    id='pet_name'
+                    name='pet_name'
+                    type='text'
+                    value={formik.values.pet_name}
+                    onChange={formik.handleChange}
+                />
+            </div>
+            <div>
+                {formik.errors.pet_name && formik.touched.pet_name && (
+                    <div>{ formik.errors.pet_name}</div>
+                )}
+            </div>
+            <div>
+                <label htmlFor='pet_type'>
+                    <div>
+                    <select
+                        id='pet_type'
+                        name='pet_type'
+                        value={formik.values.pet_type}
+                        onChange={formik.handleChange}>
+                        
+                        <option value=''>Select one</option>
+                        <option value='cat'>Cat</option>
+                        <option value='dog'>Dog</option>
+                        <option value='bird'>Bird</option>
 
+                        </select>
+                        </div>
+                    <div>
+                        {formik.errors.pet_type && formik.touched.pet_type && (
+                            <div>{ formik.errors.pet_type}</div>
+                        )}
+                    </div>
 
+                </label>
+            </div>
+            <div>
+                <label htmlFor='zip_code'>Zip code</label>
+                <input
+                    id='zip_code'
+                    name='zip_code'
+                    type='text'
+                    value={formik.values.zip_code}
+                    onChange={formik.handleChange}
+                />
+            </div>
+            <div>
+                {formik.errors.zip_code && formik.touched.zip_code && (
+                    <div>{ formik.errors.zip_code}</div>
+                )}
+            </div>
+            <div><button type='submit'>Sign Up</button></div>
 
-    
-
-
-}
+        </form>
+    )
+    }
 
 export default SignupForm
